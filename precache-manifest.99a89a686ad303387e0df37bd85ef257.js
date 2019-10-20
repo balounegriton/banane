579059b445c69a4b7e4c01eafa3dbb84f8258e2e
Bob <noeprovost@hotmail.com>
2019-10-20 13:51:50 -0400

    Updates

---
+++ precache-manifest.99a89a686ad303387e0df37bd85ef257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73fa2a9f50c8481b49506e902f7de61",
+    "url": "/banane/index.html"
+  },
+  {
+    "revision": "151af7052c1dfd302a42",
+    "url": "/banane/static/css/main.8eb8fbc5.chunk.css"
+  },
+  {
+    "revision": "e80360001e8cd214fc2c",
+    "url": "/banane/static/js/2.192c40a2.chunk.js"
+  },
+  {
+    "revision": "151af7052c1dfd302a42",
+    "url": "/banane/static/js/main.4f466d79.chunk.js"
+  },
+  {
+    "revision": "9aaa9b5d4c49a8224fb5",
+    "url": "/banane/static/js/runtime-main.7b0c9676.js"
+  },
+  {
+    "revision": "21d1a755bcac4dc530db8c2161d55ae1",
+    "url": "/banane/static/media/logo.21d1a755.png"
+  },
+  {
+    "revision": "696bc775a92f8acf42eb3c3fab9405e4",
+    "url": "/banane/static/media/loop.696bc775.mp4"
+  }
+]);
