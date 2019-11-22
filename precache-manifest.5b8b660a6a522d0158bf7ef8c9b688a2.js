843c934ffcb31ce42616a5e22374b3a1580ba465
balounegriton <provostnoe@gmail.com>
2019-11-22 16:38:34 -0500

    Updates

---
+++ precache-manifest.5b8b660a6a522d0158bf7ef8c9b688a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac9d698e16ff20b2bfe989714c91663",
+    "url": "/banane/index.html"
+  },
+  {
+    "revision": "1d59dcbce33ee1deb716",
+    "url": "/banane/static/css/main.76f3f303.chunk.css"
+  },
+  {
+    "revision": "274972e4ce423b356da1",
+    "url": "/banane/static/js/2.a2a4d2c9.chunk.js"
+  },
+  {
+    "revision": "1d59dcbce33ee1deb716",
+    "url": "/banane/static/js/main.dbee32ee.chunk.js"
+  },
+  {
+    "revision": "9aaa9b5d4c49a8224fb5",
+    "url": "/banane/static/js/runtime-main.7b0c9676.js"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/banane/static/media/Gotham-Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "23db97139feddf3d99fc676ddb418219",
+    "url": "/banane/static/media/Gotham-Light.23db9713.otf"
+  },
+  {
+    "revision": "cc05237b03aaf541077f9a5f726ba48d",
+    "url": "/banane/static/media/logo.cc05237b.png"
+  },
+  {
+    "revision": "696bc775a92f8acf42eb3c3fab9405e4",
+    "url": "/banane/static/media/loop.696bc775.mp4"
+  }
+]);
