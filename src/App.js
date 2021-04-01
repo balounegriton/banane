99ca81750aa1c2c801d0c549a332b6b698d3e93d
balounegriton <provostnoe@gamil.com>
2021-04-01 16:31:23 -0400

    new site

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './component/Navbar.js'
-import Landing from './component/Landing.js'
-import Service from './component/Service.js'
-import Contact from './component/Contact.js'
-import Message from './component/Message.js'
+
 
 class App extends Component {
   constructor() {
     super();
-    this.myRef = React.createRef()
-    this.state = {scrollTop: 0,
-      languageFr: true}
 
+    this.state = {
 
+    }
   }
-
-  goFr = () => {
-    this.setState({ languageFr: true })
-  }
-  goAn = () => {
-    this.setState({ languageFr: false})
-  
-  }
-
 
 
 
@@ -36,16 +22,7 @@ class App extends Component {
 
       return (
         <div className="App"  >
-          <Navbar languageFr={this.state.languageFr} goAn={this.goAn} goFr={this.goFr} />
-          <Landing scrollToDemo={this.scrollToDemo}  />
-          <Message languageFr={this.state.languageFr}  ref={(section) => { this.Message = section; }}/>
-          <Service languageFr={this.state.languageFr}  />
-          
-          {/* <Client /> */}
-  
-  
-  
-          <Contact />
+ allo
         </div>
       )
     
