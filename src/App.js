dc932162e879b9561f3cc7e441900fc8667f8f7e
balounegriton <provostnoe@gmail.com>
2021-08-15 22:23:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Cover5 from './component/Cover5.js'
 
 let reactSwipeEl;
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// allo First  we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
