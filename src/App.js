456f1ea036311622dd35e26d63eb81ad4adacddb
balounegriton <provostnoe@gmail.com>
2021-11-17 18:35:44 -0500

    gros erease

--- src/App.js
+++ src/App.js
@@ -1,213 +1,285 @@
-import './App.css';
 import React, { Component } from 'react';
-import ReactSwipe from 'react-swipe';
-import Cover0 from './component/Cover0.js'
-import Cover1 from './component/Cover1.js'
-
-
-import Cover2 from './component/Cover2.js'
-import Cover3 from './component/Cover3.js'
-import Cover4 from './component/Cover4.js'
-import Cover5 from './component/Cover5.js'
+import './App.css';
+import logo from "./image/logoBanane.png";
 
 
 
-
-
-let reactSwipeEl;
-
-// allo First  we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
-class App extends Component {
+class Cover0 extends Component {
   constructor() {
     super();
-
     this.state = {
-      active: 0,
-      go:true,
+      demo: false,
+      apropos: false,
+      logo: true,
+      contact: false
 
 
     };
   }
 
 
-  next = () => { reactSwipeEl.next() }
 
-  prev = () => { reactSwipeEl.prev() }
-
-  letsGo = (x) => {
-    if (x === reactSwipeEl.getPos()) {
-
-      reactSwipeEl.slide(0, 500)
-    }
-    else {
-      reactSwipeEl.slide(x, 500)
-    }
-  }
+  demoReel = () => {
 
 
-  update = () => {
-if(reactSwipeEl.getPos()===0){
-  document.getElementById("footer").className = "footer "
-}
-if(reactSwipeEl.getPos()===1){
-  document.getElementById("footer").className = "footer jcpColor"
-}
-if(reactSwipeEl.getPos()===2){
-  document.getElementById("footer").className = "footer AGcolor"
-}
-if(reactSwipeEl.getPos()===3){
-  document.getElementById("footer").className = "footer SDL"
-}
-if(reactSwipeEl.getPos()===4){
-  document.getElementById("footer").className = "footer  Pneu"
-}
-if(reactSwipeEl.getPos()===5){
-  document.getElementById("footer").className = "footer stacolor"
-}
-    this.setState({
-      active: reactSwipeEl.getPos()
-    }
-    )
+    if (this.state.demo === true && this.state.apropos === false && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: true,
+        contact: false
+      }
+      )
+      this.setState({
+        playing: false,
+      }
+      )
 
-  }
-
-  client1 = () => {
-    if (this.state.active !== 1) {
-      document.getElementById("0001").className = "clientName"
+      document.getElementById("texteDemo").className = "notHover"
+      document.getElementById("demoBox").className = "test4 displayNone"
     }
 
-  }
+    if (this.state.demo === false && this.state.apropos === true && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: true,
+        apropos: false,
+        logo: false,
+        contact: false
+      }
+      )
 
-  client1Off = () => {
-    document.getElementById("0001").className = " noDisplay"
-  }
+      document.getElementById("texteDemo").className = "Hover"
+      document.getElementById("texteApro").className = "notHover"
+      document.getElementById("demoBox").className = "test4 "
+    }
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === true && this.state.contact === false) {
+      this.setState({
+        demo: true,
+        apropos: false,
+        logo: false,
+        contact: false
+      }
+      )
 
-  client2 = () => {
-    if (this.state.active !== 2) {
-      document.getElementById("0002").className = "clientName"
+      document.getElementById("texteDemo").className = "Hover"
+      document.getElementById("demoBox").className = "test4"
+
+    }
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === false && this.state.contact === true) {
+      this.setState({
+        demo: true,
+        apropos: false,
+        logo: false,
+        contact: false
+
+      }
+      )
+      document.getElementById("texteContact").className = "notHover"
+      document.getElementById("texteDemo").className = "Hover"
+      document.getElementById("demoBox").className = "test4"
+
     }
 
+
   }
 
-  client2Off = () => {
-    document.getElementById("0002").className = " noDisplay"
-  }
+  contact = () => {
 
-  client3 = () => {
-    if (this.state.active !== 3) {
-      document.getElementById("0003").className = "clientName"
+
+    if (this.state.demo === true && this.state.apropos === false && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: false,
+        contact: true
+      }
+      )
+      this.setState({
+        playing: false,
+      }
+      )
+      document.getElementById("texteContact").className = "Hover"
+      document.getElementById("texteDemo").className = "notHover"
+      document.getElementById("demoBox").className = "test4 displayNone"
     }
 
-  }
+    if (this.state.demo === false && this.state.apropos === true && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: false,
+        contact: true
+      }
+      )
 
-  client3Off = () => {
-    document.getElementById("0003").className = " noDisplay"
-  }
-  client4 = () => {
-    if (this.state.active !== 4) {
-      document.getElementById("0004").className = "clientName"
+      document.getElementById("texteContact").className = "Hover"
+      document.getElementById("texteApro").className = "notHover"
+
+    }
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === true && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: false,
+        contact: true
+      }
+      )
+
+      document.getElementById("texteContact").className = "Hover"
+
+
     }
 
-  }
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === false && this.state.contact === true) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: true,
+        contact: false
+      }
+      )
 
-  client4Off = () => {
-    document.getElementById("0004").className = " noDisplay"
-  }
-  client5 = () => {
-    if (this.state.active !== 5) {
-      document.getElementById("0005").className = "clientName"
+      document.getElementById("texteContact").className = "notHover"
+
+
     }
 
+
+
   }
 
-  client5Off = () => {
-    document.getElementById("0005").className = " noDisplay"
+
+  apropo = () => {
+
+    if (this.state.demo === true && this.state.apropos === false && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: true,
+        logo: false,
+        contact: false
+      }
+      )
+
+      document.getElementById("texteApro").className = "Hover"
+      document.getElementById("texteDemo").className = "notHover"
+      document.getElementById("demoBox").className = "test4 displayNone"
+      this.setState({
+        playing: false,
+      }
+      )
+
+    }
+
+    if (this.state.demo === false && this.state.apropos === true && this.state.logo === false && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: false,
+        logo: true,
+        contact: false
+      }
+      )
+
+      document.getElementById("texteDemo").className = "notHover"
+      document.getElementById("demoBox").className = "test4 displayNone"
+      document.getElementById("texteApro").className = "notHover"
+      this.setState({
+        playing: false,
+      }
+      )
+    }
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === true && this.state.contact === false) {
+      this.setState({
+        demo: false,
+        apropos: true,
+        logo: false,
+        contact: false
+      }
+      )
+
+      document.getElementById("texteApro").className = "Hover"
+
+    }
+
+    if (this.state.demo === false && this.state.apropos === false && this.state.logo === false && this.state.contact === true) {
+      this.setState({
+        demo: false,
+        apropos: true,
+        logo: false,
+        contact: false
+
+      }
+      )
+      document.getElementById("texteContact").className = "notHover"
+      document.getElementById("texteApro").className = "Hover"
+
+
+    }
+
+
+
+
   }
+
+
+
 
   render() {
-   
-
-   
-   
-   
-   
 
     return (
+      <div className="box2">
+        <div className="test5">
 
-      <div className="App">
-
-        <div >
-       
-          <ReactSwipe
-            className="width100"
-            swipeOptions={{
-              continuous: true,
-              disableScroll: false,
-              callback: this.update,
-            }}
-            ref={el => (reactSwipeEl = el)}  >
+          <div id="demoBox" className="test4 displayNone">
+            <div className="test2">
 
 
-            <div className="width100"><Cover0 className="width100" dataFromParent={this.state.active} /></div>
-            <div className="width100"><Cover1 className="width100" dataFromParent={this.state.active} /></div>
-            <div className="width100"><Cover2 className="width100" dataFromParent={this.state.active} /></div>
-            <div className="width100"><Cover3 className="width100" dataFromParent={this.state.active} /></div>
-            <div className="width100"><Cover4 className="width100" dataFromParent={this.state.active} /></div>
-            <div className="width100"><Cover5 className="width100" dataFromParent={this.state.active} /></div>
 
-          </ReactSwipe>
+
+
+            </div>
+          </div>
+
+          {this.state.logo && <img className="logoLanding" id="logoL" src={logo} alt="logoL" />}
+          {this.state.apropos &&
+
+            <div>
+              <div className="texteApro"  >La Banane est un studio créatif plein services, basé à Bromont, Qc. Nous sommes spécialisés
+                dans les publicités TV et web. Nous offrons un service de consultation qui cible vos besoins afin de proposer
+                une solution éfficace adaptée à vos besoins. Nos experts multidiciplinaires ne reculent devant
+                rien pour livrer la marchandise.Nous offrons un service de consultation qui cible vos besoins afin de proposer
+                une solution éfficace adaptée à vos besoins. Nos experts multidiciplinaires ne reculent devant
+                rien pour livrer la marchandise.
+              </div>
+
+            </div>
+          }
+
+          {this.state.contact && <div>   contact </div>}
+
+          {this.state.demo && <div>  <iframe className="" id="" title="" src="https://player.vimeo.com/video/248658478" width="640" height="360" frameBorder="0" webkitallowfullscreen="true"
+              mozallowfullscreen="true" allowFullScreen></iframe> </div>}
 
 
         </div>
 
-
-
-
-
-
-
-
-
-        <div id="footer" className="footer" >
-          <div id="001" className="client" onClick={() => this.letsGo(1)} onMouseEnter={this.client1} onMouseLeave={this.client1Off}  >
-            <div id="0001" className="clientName noDisplay">
-              JCPerreault
-            </div>
-          </div>
-          <div id="002" className="client" onClick={() => this.letsGo(2)} onMouseEnter={this.client2} onMouseLeave={this.client2Off} >
-          <div id="0002" className="clientName noDisplay">
-              Adrien Gagnon
-            </div>
-          </div>
-          <div id="003" className="client" onClick={() => this.letsGo(3)} onMouseEnter={this.client3} onMouseLeave={this.client3Off}>
-          <div id="0003" className="clientName noDisplay">
-              Sentiers de l&rsquo;est
-            </div>
-          </div>
-          <div id="004" className="client" onClick={() => this.letsGo(4)} onMouseEnter={this.client4} onMouseLeave={this.client4Off}>
-          <div id="0004" className="clientName noDisplay">
-             Pneu select
-            </div>
-          </div>
-          <div id="005" className="client" onClick={() => this.letsGo(5)} onMouseEnter={this.client5} onMouseLeave={this.client5Off}>
-          <div id="0005" className="clientName noDisplay">
-            station 22
-            </div>
-          </div>
+        <div className="texteDemo">
+          <div id="texteDemo" className="notHover" onClick={this.demoReel} >demo reel  </div>
         </div>
+
+
+        <div className="apropos">
+          <div id="texteApro" className="notHover" onClick={this.apropo}>à propos  </div>
+
+        </div>
+        <div className="texteContact">
+          <div id="texteContact" className="notHover" onClick={this.contact} > contact </div>
+        </div>
+
+
 
       </div>
 
-
-
-    )}
-  
+    )
+  }
 }
-
-export default App;
-
+export default Cover0;
