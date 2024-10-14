bef1f392738c32aafe9e9a5bc44d2a3c4b1bb198
balounegriton <provostnoe@gmail.com>
2024-10-14 11:42:49 -0400

    mnew`

--- src/component/logo.js
+++ src/component/logo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../App.css';
-import logo from "../image/logoBanane.png";
 import VideoBackground from './VideoBackground.js';
 import PopUp from './Popup.js';
 
